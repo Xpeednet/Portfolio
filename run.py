22c48027f02d472e5f6e1d22fccf7bdd74771e87
Xpeednet <129681844+Xpeednet@users.noreply.github.com>
2023-08-01 18:36:40 -0300

    Primeras rutas de prueba en FLASK!

---
+++ run.py
@@ -0,0 +1,18 @@
+#Importar componentes
+from flask import Flask
+
+#Crear app medante instancia de clase Flask
+app=Flask(__name__)
+
+#Rutas de la aplicación
+@app.route('/')
+def holamundo():
+    return 'Hola Mundo'
+
+@app.route('/misproyectos')
+def misproyectos():
+    return 'Mis proyectos'
+
+#Ejecucion de la aplicacion
+if __name__ == '__main__':
+    app.run(debug=True)

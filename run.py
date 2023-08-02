00b7910c7bfebd367e0d6c45230f96d339abb274
Xpeednet <129681844+Xpeednet@users.noreply.github.com>
2023-08-01 21:32:33 -0300

    Modificacion templates para usar template base.html, arreglo controlador ruta / para aceptar GET y POST

--- run.py
+++ run.py
@@ -1,25 +1,29 @@
 #Importar componentes
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 #Crear app medante instancia de clase Flask
 app=Flask(__name__)
 
 #Rutas de la aplicación
-@app.route('/', methods=['GET'])
-def holamundo():
-    return render_template('index.html')
-
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        nombre = request.form['Nombre']
+        return render_template('/index.html', nombre = nombre)
+    else:
+        return render_template('/index.html')
+    
 @app.route('/proyectos', methods=['GET'])
-def misproyectos():
-    return render_template('proyectos.html')
+def proyectos():
+    return render_template('/proyectos.html')
 
 @app.route('/blog', methods=['GET'])
-def miblog():
-    return render_template('blog.html')
+def blog():
+    return render_template('/blog.html')
 
 @app.route('/contacto', methods=['GET'])
-def misproyectos():
-    return render_template('contacto.html')
+def contacto():
+    return render_template('/contacto.html')
 
 #Ejecucion de la aplicacion
 if __name__ == '__main__':
